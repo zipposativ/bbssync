75f30eb814017acc2ecc6a982bf00ba9be730d9a
zipposativ <104485696+zipposativ@users.noreply.github.com>
2026-01-15 11:10:27 +0100

    add

--- www/bbssync/src/ad.php
+++ www/bbssync/src/ad.php
@@ -304,6 +304,7 @@
         ldap_mod_replace($ldap, $new_user_dn, $entry);
         //change sAMAccountName
         $value["samaccountname"] = $samaccountname;
+        $value["userprincipalname"] = $samaccountname."@".$domain;
         ldap_mod_replace($ldap, $new_user_dn, $value);
         return true;
       }else{
